b03b6cc2860972fa154305448ee3543ebff3cbdb
Andre Almar <andre@y7mail.com>
2017-12-18 14:32:26 -0200

    using variables

--- app.rb
+++ app.rb
@@ -9,5 +9,15 @@ configure :development do
 end
 
 get '/' do
-    'Hello World!'
+    @usernames = [ 'ssaunier', 'Papillard' ]
+    erb :index
+  end
+
+get '/about' do
+    erb :about
+end
+
+get '/team/:username' do
+    puts params[:username]
+    "The username is #{params[:username]}"
 end
